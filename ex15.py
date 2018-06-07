e2b9afb92651a7ae99fb8633cd2ef69d21fdb750
ShunLeiNadiMoe <shunleinadi2234@gmail.com>
2018-06-06 20:16:35 -0700

    My fifteen python exercises

--- ex15.py
+++ ex15.py
@@ -4,12 +4,13 @@ script, filename = argv
 
 txt = open(filename)
 
-print(f"Here's your file {filename}:"
-print(f"{txt.read()}")
+print(f"Here's your file {filename}:")
+print(txt.read())
 
 print("Type the filename again:")
-file_again=input("> ")
+file_again = input("> ")
 
-txt_again=open(file_again)
+txt_again = open(file_again)
 
 print(txt_again.read())
+
